dc658e58ed20e149b111c3f1272cdc6fc5f30a8e
Vasyl Diachuk <djachukvasja@gmail.com>
2024-03-04 21:31:10 +0200

    222

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -3,6 +3,7 @@ import { logOut } from '../../redux/author/operations';
 import { useAuth } from '../../hooks';
 import css from './UserMenu.module.css';
 import { Button, Typography } from '@mui/material';
+import { FaRegCircleUser } from 'react-icons/fa6';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
@@ -10,15 +11,15 @@ const UserMenu = () => {
   const { user } = useAuth();
 
   return (
-    <>
+    <div className={css.wrapper}>
       <Typography sx={{ mr: 5 }} className={css.username}>
-        Welcome, {user.name}
+        <FaRegCircleUser size="22" /> {user.name}
       </Typography>
 
       <Button color="inherit" variant="text" onClick={() => dispatch(logOut())}>
         Logout
       </Button>
-    </>
+    </div>
   );
 };
 
