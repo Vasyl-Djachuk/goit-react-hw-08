cdcd7e37dadd27be2f8ddf5f2e96feab694d11c5
Vasyl Diachuk <djachukvasja@gmail.com>
2024-03-04 21:01:42 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,11 +6,19 @@ import PrivateRoute from './PrivateRoute';
 import RestrictedRoute from './RestrictedRoute';
 import { refreshUser } from '../redux/author/operations';
 import { useAuth } from '../hooks';
+import css from './App.module.css';
+import Loader from 'react-spinners/PuffLoader';
 
 const HomePage = lazy(() => import('../pages/Home/Home'));
 const RegisterPage = lazy(() => import('../pages/Register/Register'));
 const LoginPage = lazy(() => import('../pages/Login/Login'));
 const ContactsPage = lazy(() => import('../pages/Contacts/Contacts'));
+
+const override = {
+  display: 'block',
+  margin: '0 auto',
+  borderColor: 'red',
+};
 
 const App = () => {
   const dispatch = useDispatch();
@@ -20,39 +28,61 @@ const App = () => {
     dispatch(refreshUser());
   }, [dispatch]);
 
+  const color = '#6aabe4';
   return (
-    !isRefreshing && (
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {!isRefreshing ? (
+          <>
+            <Route index element={<HomePage />} />
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute
+                  redirectTo="/contacts"
+                  component={<RegisterPage />}
+                />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute
+                  redirectTo="/contacts"
+                  component={<LoginPage />}
+                />
+              }
+            />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute
+                  redirectTo="/login"
+                  component={<ContactsPage />}
+                />
+              }
+            />
+            <Route path="*" element={<HomePage />} />
+          </>
+        ) : (
           <Route
-            path="/register"
+            path="*"
             element={
-              <RestrictedRoute
-                redirectTo="/contacts"
-                component={<RegisterPage />}
-              />
+              <div className={css.loader}>
+                <Loader
+                  color={color}
+                  loading={isRefreshing}
+                  cssOverride={override}
+                  size={100}
+                  aria-label="Loading Spinner"
+                  data-testid="loader"
+                />
+              </div>
             }
           />
-          <Route
-            path="/login"
-            element={
-              <RestrictedRoute
-                redirectTo="/contacts"
-                component={<LoginPage />}
-              />
-            }
-          />
-          <Route
-            path="/contacts"
-            element={
-              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
-            }
-          />
-          <Route path="*" element={<HomePage />} />
-        </Route>
-      </Routes>
-    )
+        )}
+      </Route>
+    </Routes>
   );
 };
 
