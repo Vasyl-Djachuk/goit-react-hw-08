7532e90b49d07c6a26cddb9ff002b1a07ad86471
Vasyl Diachuk <djachukvasja@gmail.com>
2024-03-04 21:11:36 +0200

    1

--- src/components/BackdropLoder/BackdropLoder.jsx
+++ src/components/BackdropLoder/BackdropLoder.jsx
@@ -3,18 +3,15 @@ import CircularProgress from '@mui/material/CircularProgress';
 import { useSelector } from 'react-redux';
 
 import { selectLoading } from '../../redux/contacts/selectors';
-import { useAuth } from '../../hooks';
 
 const BackdropLoder = () => {
   const loding = useSelector(selectLoading);
-
-  const { isRefreshing } = useAuth();
 
   const handleClose = () => {};
   return (
     <Backdrop
       sx={{ color: '#fff', zIndex: theme => theme.zIndex.drawer + 1 }}
-      open={loding || isRefreshing}
+      open={loding}
       onClick={handleClose}
     >
       <CircularProgress color="inherit" />
